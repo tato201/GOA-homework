cc50680e4b3ec92c9563eaa149c9c1e722596cb9
tato201 <tatochigoga0@gmail.com>
2025-04-26 15:57:22 +0400

    commit

--- day 01/homework/hm.py
+++ day 01/homework/hm.py
@@ -1,2 +1,20 @@
 name="tato chigoga"
-color="black"
+
+color="black, green"
+color="white"
+
+integer_age=20
+integer_age=21
+integer_age=22
+
+family_name="zaza chigoga"
+family_name="guguli shengelia"
+family_name="nata chigoga"
+family_name="giorgi cxvaradze"
+
+boolean_pet=True
+boolean_bird=False
+
+#(string) monacemta ertbolioba saxelebit, gvarebit, samsaxuris tipebit da ase shemdeg
+#(integer) inaxavs monacemebs cifrebis shesaxeb romlebic sheidzleba iyos negatiuri da dadebiti ricxvta ertoblioba
+#(boolean) azustebs an uaryobs arsebul teqsts tu kitxvas
