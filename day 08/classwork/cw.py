6669c2e044dcfbf48f8e2632b31a2431c5ceae3a
tato201 <tatochigoga0@gmail.com>
2025-05-22 18:08:36 +0400

    commit

---
+++ day 08/classwork/cw.py
@@ -0,0 +1,5 @@
+for i in range(0,10,1):
+    print(i)
+
+#davaleba 3
+#tavi aris pirvel xazze mocemuli kodi xolo meore xazze mocemuli ki aris sxelui
---
+++ day 08/classwork/cw1.py
@@ -0,0 +1,5 @@
+for i in range(0, 5 , 1):
+    name = input(f"{i + 1}) enter your name ") 
+print(f"{name}, ")
+
+                 
