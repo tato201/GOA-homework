160206ca12fe144d6f27d2dd39c6e30cad385f65
tato201 <tatochigoga0@gmail.com>
2025-07-05 00:21:49 +0400

    commit

---
+++ day 19/classwork/cw.py
@@ -0,0 +1,10 @@
+# indexing გვეხმარება რომ ლისტებიდან ამოვირჩიოთ ერთი ნებისმიერი ტიპის მონაცემი.
+
+cars = ["toyota" , "BMW" , "mercedes" , "audi" , "alfa romeo"]
+cars[2] = "nissan"
+cars[4] = "mazda"
+print(cars[0])
+print(cars[1])
+print(cars[2])
+print(cars[3])
+print(cars[4])
---
+++ day 19/homework/hm.py
@@ -0,0 +1,14 @@
+numbers = [10, 20, 30, 40, 50, 60, 70]
+print(numbers[0:3])
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f']
+print(letters[2:5])
+
+words = ['apple', 'banana', 'cherry', 'date', 'fig', 'grape']
+print(words[4:])
+
+colors = ['red', 'green', 'blue', 'yellow', 'purple']
+print(colors[2:])
+
+text = "programming"
+print(text[0:5])
